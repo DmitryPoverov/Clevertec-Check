16559d901375ece34742cc08bb944698f3b2d1d8
DmitryPoverov <did.ink@mail.ru>
2022-07-28 16:20:33 +0300

    Corrected: Check, Servlet, DBConnection, NewCheck

--- src/main/java/ru/clevertec/jdbc/entities/NewCheck.java
+++ src/main/java/ru/clevertec/jdbc/entities/NewCheck.java
@@ -1,15 +1,28 @@
 package ru.clevertec.jdbc.entities;
 
+import ru.clevertec.console.Check;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class NewCheck {
+public class NewCheck extends Check {
 
     private static final NewCheck INSTANCE = new NewCheck();
+
     private List<Product> productList = new ArrayList<>();
     private DiscountCard discountCard = new DiscountCard();
 
     private NewCheck() {
+        super();
+    }
+
+    public NewCheck(String[] args) {
+        super(args);
+    }
+
+    public NewCheck(String path) throws IOException {
+        super(path);
     }
 
     public static NewCheck getInstance() {
--- src/main/java/ru/clevertec/jdbc/utils/DBConnection.java
+++ src/main/java/ru/clevertec/jdbc/utils/DBConnection.java
@@ -10,6 +10,8 @@ public class DBConnection {
     private static final String DB_USER = PropertyManager.getUser();
     private static final String DB_PASSWORD = PropertyManager.getPassword();
 
+    //TODO private constructor
+
     public static Connection getConnection() throws SQLException {
         Connection connection;
         ProxyConnection proxyConnection;
